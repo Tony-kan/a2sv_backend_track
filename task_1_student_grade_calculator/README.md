f551f364e40c5d396f84bba554147d21d0fe4792
Tony Kanyamuka <tonykanyamuka@gmail.com>
2025-04-04 21:09:22 +0200

    implement the task_1 : student average grade calculator

---
+++ task_1_student_grade_calculator/README.md
@@ -0,0 +1,27 @@
+
+## Fundamentals of Go Tasks
+
+### Task: Student Grade Calculator
+
+This task involves creating a Go console application that allows students to calculate their average grade based on different subjects.
+
+#### Requirements:
+- **Input**:
+  - Prompt the student to enter their name.
+  - Ask for the number of subjects they have taken.
+  - For each subject, collect the subject name and the grade obtained (numeric value).
+- **Output**:
+  - Display the student's name.
+  - Show individual subject grades.
+  - Calculate and display the average grade.
+
+#### Implementation Details:
+- Use variables and data types to store student data.
+- Use conditional statements to validate input (e.g., ensure grade values are within a valid range).
+- Implement loops to handle multiple subjects and grades.
+- Utilize collections (e.g., List, Dictionary) to store subject names and corresponding grades.
+- Define a method to calculate the average grade based on the entered grades.
+- Use string interpolation to display the results in a user-friendly format.
+- Optionally, write tests for the code to ensure correctness.
+
+This task is designed to strengthen your understanding of Go fundamentals, including variables, loops, conditionals, and collections.
---
+++ task_1_student_grade_calculator/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+)
+
+var grades = map[string]int{}
+
+func main() {
+	// fmt.Println("Hello, World!")
+	var studentName string
+	var noOfSubjects int
+
+	fmt.Print("Enter the your student name : ")
+	fmt.Scanln(&studentName)
+
+	fmt.Print("How many subjects do  you take ? : ")
+	fmt.Scanln(&noOfSubjects)
+
+	getSubjects(noOfSubjects)
+
+	// getAverageGrade();
+	fmt.Println("------------------------------------------------------")
+
+	fmt.Printf("Your Student name is : %v \n No of subjeect : %v \n", studentName, noOfSubjects)
+
+	fmt.Printf("Your Average Grade is : %v", getAverageGrade())
+
+	fmt.Println("\n------------------------------------------------------")
+
+}
+
+func getSubjects(noOfSubjects int) map[string]int {
+	var subjectName string
+	var grade int
+
+	if noOfSubjects < 1 {
+		fmt.Println("You have to take at least one subject")
+		return nil
+	}
+	for i := 1; i <= noOfSubjects; i++ {
+		fmt.Printf("Enter the name of subject %d : ", i)
+		fmt.Scanln(&subjectName)
+
+		fmt.Printf("Enter the grade for %s : ", subjectName)
+		fmt.Scanln(&grade)
+
+		grades[subjectName] = grade
+	}
+	return grades
+	// fmt.Println("\n number of the subjects :", noOfSubjects)
+}
+
+func getAverageGrade() float64 {
+	var count, total int
+
+	for _, grade := range grades {
+		count++
+		total += grade
+	}
+
+	return float64(total) / float64(count)
+
+}
