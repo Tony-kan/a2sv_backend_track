710088d53caf4f07c9e19df4ab51278a811f9ce5
Tony Kanyamuka <tonykanyamuka@gmail.com>
2025-04-20 23:33:34 +0200

    mongo_tutorial : implemented crud operations

--- mongo_db_tutorial/main.go
+++ mongo_db_tutorial/main.go
@@ -5,14 +5,14 @@ import (
 	// "fmt"
 
 	"context"
+	"fmt"
+	"go.mongodb.org/mongo-driver/bson"
 	"log"
 	"os"
 
 	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
-	// "go.mongodb.org/mongo-driver/mongo"
-	// "go.mongodb.org/mongo-driver/v2/x/mongo/driver/mongocrypt/options"
 )
 
 type Trainer struct {
@@ -24,7 +24,6 @@ type Trainer struct {
 func main() {
 	godotenv.Load()
 	mongo_uri := os.Getenv("MONGO_URI")
-
 
 	// set client options
 	clientOptions := options.Client().ApplyURI(mongo_uri)
@@ -45,13 +44,108 @@ func main() {
 
 	log.Println("Connected to MongoDB!")
 
+	collection := client.Database("test").Collection("trainers")
+
 	// collection := client.Database("test").Collection("trainers")
+	ash := Trainer{"Ash", 10, "Pallet Town"}
+	misty := Trainer{"Misty", 12, "Cerun Town"}
+	brock := Trainer{"Brock", 15, "Pewter Town"}
 
-	err = client.Disconnect(context.TODO())
+	// Data insertion to db
+	// Insert a single document
+	insertResult, err := collection.InsertOne(context.TODO(), ash)
 
+	// insertResult,err :=
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	log.Println("Connection to Mongodb closed.")
+	log.Println("Inserted ash document : ", insertResult.InsertedID)
+
+	// Insert many documents
+	trainers := []interface{}{misty, brock}
+
+	insertManyResult, err := collection.InsertMany(context.TODO(), trainers)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Inserted trainers : ", insertManyResult.InsertedIDs)
+
+	// update document
+	filter := bson.D{{"name", "Ash"}}
+
+	update := bson.D{
+		{"$inc", bson.D{
+			{"age", 1},
+		}},
+	}
+
+	updateResult, err := collection.UpdateOne(context.TODO(), filter, update)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("Matched %v documents and updated %v documents. \n", updateResult.MatchedCount, updateResult.ModifiedCount)
+
+	// find documents
+
+	// create a value into which the result can be decoded
+	var result Trainer
+
+	err = collection.FindOne(context.TODO(), filter).Decode(&result)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("Found a single document : %+v\n", result)
+
+	// finding multiple documents
+	findOptions := options.Find()
+	findOptions.SetLimit(2)
+
+	// Here's an array in which you can store the decoded documents
+	var results []*Trainer
+
+	// Passing bson.D{{}} as the filter matches all documents in the collection
+	cur, err := collection.Find(context.TODO(), bson.D{{}}, findOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// finding multiple documents returns a cursor
+	// iterating through the cursor allows us to decode documents one at a time
+	for cur.Next(context.TODO()) {
+		// create a value into which the single document can be decoded
+		var elem Trainer
+		err := cur.Decode(&elem)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		results = append(results, &elem)
+	}
+
+	if err := cur.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	// close the curso once finished
+	cur.Close(context.TODO())
+
+	fmt.Printf("Found multiple documents (array of pointers): %v\n", results)
+
+	// delete documents
+	deleteResult, err := collection.DeleteMany(context.TODO(), bson.D{{}})
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("Deleted %v documents in the trainers collection\n", deleteResult.DeletedCount)
+
+	// disconnect
+	//err = client.Disconnect(context.TODO())
+	//
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	//
+	//log.Println("Connection to Mongodb closed.")
 }
